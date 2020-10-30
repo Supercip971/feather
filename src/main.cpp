b075b07e2de2220b207bd6bcb47ee8735930c523
Supercip971 <supercyp971@gmail.com>
2020-10-30 11:33:59 -0400

    [other] now read file

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,10 @@
 #include "feather_map.h"
 #include "feather_type.h"
 #include "feather_vm.h"
-
 #include "feather_vector.h"
 #include <stdio.h>
+#define PROGRAMM_INTERPRET
 /*
-
-
 0 | func myfunction(){
 1 |     int v = 1 + 3 * 4;
 2 | }
@@ -15,11 +13,35 @@
 4 | func main(){
 5 |     myfunction();
 6 | }
-
-
-
-
 */
+#ifdef PROGRAMM_INTERPRET
+#include <fstream>
+#include <iostream>
+int main(int argc, const char* argv[]){
+    for(int i = 1; i < argc; i++){
+        std::ifstream stream = std::ifstream(argv[i], std::ios::in | std::ios::binary);
+        if(!stream.is_open()){
+            printf("error file %s can not be opened \n", argv[i]);
+        }
+        stream.seekg(0, std::ios::end);
+        uint64_t length = stream.tellg();
+        char* dat = new char[length];
+        stream.seekg(0, std::ios::beg);
+        stream.read(dat, length);
+
+        fsl::feather_virtual_machine vm = fsl::feather_virtual_machine(dat);
+        vm.start();
+
+        stream.close();
+        delete[] dat;
+    }
+
+
+    return 0;
+}
+
+
+#else
 int main()
 {
     const char *feather_code =
@@ -45,3 +67,4 @@ int main()
 
     return 0;
 }
+#endif
