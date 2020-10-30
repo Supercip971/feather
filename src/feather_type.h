5c67b2fdbc326a2276bd421661838344c9d17093
Supercip971 <supercyp971@gmail.com>
2020-10-30 08:48:55 -0400

    [type] added static type

--- src/feather_type.h
+++ src/feather_type.h
@@ -14,7 +14,8 @@ namespace fsl
         TYPE_DELIMITOR = 3,
         TYPE_TOKEN = 4,
         TYPE_END_OF_LINE = 5,
-        TYPE_NUMBER = 6
+        TYPE_NUMBER = 6,
+        TYPE_STATIC_STRING = 7
     };
 
     // ---------------- FEATHER OPERATOR ----------------
